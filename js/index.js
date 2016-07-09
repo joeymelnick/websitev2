ad64d1a4f1e81c028dbb256f251a0f67527adf44
Joey Melnick <jmelnick86@gmail.com>
2016-07-08 22:23:24 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function invertTwitter(){
+  $('.twitter-black').css('display', 'none');
+  $('.twitter-white').css('display', 'block')
+}
+function invertTwitter2(){
+   $('.twitter-black').css('display', 'block');
+  $('.twitter-white').css('display', 'none')
+}
+
+function invertGithub(){
+  $('.github-black').css('display', 'none');
+  $('.github-white').css('display', 'block')
+}
+function invertGithub2(){
+   $('.github-black').css('display', 'block');
+  $('.github-white').css('display', 'none')
+}
+
+function invertLinked(){
+  $('.linked-black').css('display', 'none');
+  $('.linked-white').css('display', 'block')
+}
+function invertLinked2(){
+   $('.linked-black').css('display', 'block');
+  $('.linked-white').css('display', 'none')
+}
+
+function invertEmail(){
+  $('.email-black').css('display', 'none');
+  $('.email-white').css('display', 'block')
+}
+function invertEmail2(){
+   $('.email-black').css('display', 'block');
+  $('.email-white').css('display', 'none')
+}
